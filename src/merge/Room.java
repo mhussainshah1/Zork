01253ecdffda7f0b39e4174313e17dad074a8c9d
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-06 23:32:10 -0500

    OOP

---
+++ src/merge/Room.java
@@ -0,0 +1,68 @@
+package merge;
+
+public class Room {
+    private int roomNumber;
+    private String roomName;
+    private String item;
+    private String diections;
+    private boolean found;
+    private int money;
+
+    public Room(int roomNumber, String roomName, String item, String diections) {
+        this.roomNumber = roomNumber;
+        this.roomName = roomName;
+        this.item = item;
+        this.diections = diections;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public boolean isFound() {
+        return found;
+    }
+
+    public void setFound(boolean found) {
+        this.found = found;
+    }
+
+    public int getMoney() {
+        int random = 1+(int)(Math.random() *1000);
+        setMoney(random);
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "You are in roomNumber=" + roomNumber +
+                ", roomName='" + roomName + '\'' +
+                ", item contains='" + item + '\'' +
+                ", direction & room number ='" + diections + '\'' +
+                '}';
+    }
+}
